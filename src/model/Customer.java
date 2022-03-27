a12ba9fe38bcbd994e6189561a17c6f119c87d3d
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-27 16:25:02 +0200

    Created Customer class in model layer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Customer {
     private int id;
     private String firstName;
@@ -7,5 +9,78 @@ public class Customer {
     private String address;
     private int postalCode;
     private String city;
+    private ArrayList<SaleOrder> saleOrders;
 
+    public Customer(String firstName, String lastName, String address, int postalCode, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public Customer(int id, String firstName, String lastName, String address, int postalCode, String city) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public ArrayList<SaleOrder> getSaleOrders() {
+        return saleOrders;
+    }
+
+    public void setSaleOrders(ArrayList<SaleOrder> saleOrders) {
+        this.saleOrders = saleOrders;
+    }
 }
