4b8106a3559c118cf911676f03e4c5cb7fb00993
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-27 17:50:56 +0200

    Created Product class in model layer

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Product {
+    private int id;
+    private String name;
+    private double purchasingPrice;
+    private double sellingPrice;
+    public enum WeightCategory{ONE, FIVE, TEN}
+    private WeightCategory weightCategoryOption;
+    private int weightCategory;
+
+
+    public Product(String name, double purchasingPrice, double sellingPrice, WeightCategory weightCategoryOption) {
+        this.name = name;
+        this.purchasingPrice = purchasingPrice;
+        this.sellingPrice = sellingPrice;
+        this.weightCategoryOption = weightCategoryOption;
+        switch (weightCategoryOption){
+            case ONE -> this.weightCategory = 1;
+            case FIVE -> this.weightCategory = 5;
+            case TEN -> this.weightCategory = 10;
+        }
+
+
+    }
+
+
+    public int getWeightCategory() {
+        return weightCategory;
+    }
+
+    public void setWeightCategory(WeightCategory weightCategoryOption) {
+
+        switch (weightCategoryOption){
+            case ONE -> this.weightCategory = 1;
+            case FIVE -> this.weightCategory = 5;
+            case TEN -> this.weightCategory = 10;
+        }
+    }
 }
---
+++ src/model_tests/ProductTests.java
@@ -0,0 +1,45 @@
+package model_tests;
+
+import model.Product;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import model.Product.*;
+import org.junit.jupiter.api.TestInstance;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ProductTests {
+    private Product product;
+
+    @BeforeAll
+    public void setUp(){
+        product = new Product("test", 1.4, 1.5, WeightCategory.ONE);
+    }
+
+    // something weird going on here, 2nd test should fail but doesn't
+    @Test
+    public void weightCategoryShouldBeSetToFive(){
+        product.setWeightCategory(WeightCategory.FIVE);
+
+        assertEquals(5, product.getWeightCategory());
+       // System.out.println(product.getWeightCategory() + " at " + System.nanoTime());
+    }
+
+    @Test
+    public void weightCategoryShouldBeOne(){
+
+       // System.out.println(product.getWeightCategory()  + " at " + System.nanoTime());
+        assertEquals(1, product.getWeightCategory());
+    }
+
+
+
+
+    @AfterAll
+    public void tearDown(){
+        product = null;
+    }
+}
