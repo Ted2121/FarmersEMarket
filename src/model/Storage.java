f2294e7ba96732ae17dfd0adfc91f56f4b062e40
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-03-28 16:13:22 +0200

    Created Storage class as singleton

--- src/model/Storage.java
+++ src/model/Storage.java
@@ -1,4 +1,20 @@
 package model;
 
+import java.util.HashMap;
+
+// singleton for storage
 public class Storage {
+    private static HashMap<Product, Integer> productsInStorage = null;
+
+    private Storage(){
+
+    }
+
+    public static HashMap<Product, Integer> getInstance(){
+        if(productsInStorage == null){
+            productsInStorage = new HashMap<Product, Integer>();
+        }
+
+        return productsInStorage;
+    }
 }
