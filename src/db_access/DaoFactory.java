864959cceadf8d2e60f8ca22dbcac9ffd835d14f
schesneau <10435523@ucn.dk>
2022-04-12 11:28:38 +0200

    Adding all the methods

--- src/db_access/DaoFactory.java
+++ src/db_access/DaoFactory.java
@@ -1,5 +1,40 @@
 package db_access;
 
+import db_access.DaoImplementation.*;
+import db_access.DaoInterfaces.*;
+
 public class DaoFactory{
-	//TODO
+	private DaoFactory() {};
+	
+	public static CustomerDao getCustomerDao() {
+		return new CustomerDaoImplementation();
+	}
+	
+	public static LineItemDao getLineItemDao() {
+		return new LineItemDaoImplementation();
+	}
+	
+	public static OrderDao getOrderDao() {
+		return new OrderDaoImplementation();
+	}
+	
+	public static ProductDao getProductDao() {
+		return new ProductDaoImplementation();
+	}
+	
+	public static ProductInformationDao getProductInformationDao() {
+		return new ProductInformationDaoImplementation();
+	}
+	
+	public static ProviderDao getProviderDao() {
+		return new ProviderDaoImplementation();
+	}
+	
+	public static PurchaseOrderDao getPurchaseOrderDao() {
+		return new PurchaseOrderDaoImplementation();
+	}
+	
+	public static SaleOrderDao getSaleOrderDao() {
+		return new SaleOrderDaoImplementation();
+	}
 }
