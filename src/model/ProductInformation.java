d153620669be83167b74af908e7f85d7ec789531
Kylian CONNAN <10435524@ucn.dk>
2022-04-13 12:08:58 +0200

    Make the Product information model

--- src/model/ProductInformation.java
+++ src/model/ProductInformation.java
@@ -1,40 +1,38 @@
 package model;
 
-import java.util.HashMap;
-
-// singleton for storage
 public class ProductInformation {
-//    private static HashMap<Product, StorageInfo> productsInStorage = null;
-//    private class StorageInfo{
-//        private int quantity;
-//        private static int locationCode = 10000;
-//
-//        public StorageInfo(int quantity) {
-//            this.quantity = quantity;
-//            locationCode++;
-//        }
-//
-//        public int getQuantity() {
-//            return quantity;
-//        }
-//
-//        public void setQuantity(int quantity) {
-//            this.quantity = quantity;
-//        }
-//
-//        public int getLocationCode() {
-//            return locationCode;
-//        }
-//
-//
-//    }
-//    private ProductInformation(){}
-//
-//    public static HashMap<Product, StorageInfo> getInstance(){
-//        if(productsInStorage == null){
-//            productsInStorage = new HashMap<Product, StorageInfo>();
-//        }
-//
-//        return productsInStorage;
-//    }
+	private int locationCode;
+	private int quantity;
+	private Product product;
+	
+	public ProductInformation(int locationCode, int quantity, Product product) {
+		super();
+		this.locationCode = locationCode;
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public int getLocationCode() {
+		return locationCode;
+	}
+
+	public void setLocationCode(int locationCode) {
+		this.locationCode = locationCode;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 }
