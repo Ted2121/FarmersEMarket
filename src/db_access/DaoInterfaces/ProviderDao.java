5b5b052f07d002de05a8b9ea98afd13b23777f72
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-04-13 15:23:36 +0200

    Changing from Person to Provider

--- src/db_access/DaoInterfaces/ProviderDao.java
+++ src/db_access/DaoInterfaces/ProviderDao.java
@@ -3,15 +3,27 @@ package db_access.DaoInterfaces;
 import model.Person;
 import model.Provider;
 
+<<<<<<< Updated upstream
+=======
+import model.Provider;
+>>>>>>> Stashed changes
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ProviderDao {
+<<<<<<< Updated upstream
     Provider findPersonById(int PersonId) throws SQLException;
     List<Provider> findAllPersons() throws SQLException;
     int createPerson(Provider objectToInsert) throws SQLException;
     void updatePerson(Provider objectToUpdate) throws SQLException;
     void deletePerson(Provider objectToDelete) throws SQLException;
     Provider findPersonByFullName(String fullName) throws SQLException;
+=======
+    Provider findProviderById(int ProviderId) throws SQLException;
+    List<Provider> findAllProviders() throws SQLException;
+    int createProvider(Provider objectToInsert) throws SQLException;
+    void updateProvider(Provider objectToUpdate) throws SQLException;
+    void deleteProvider(Provider objectToDelete) throws SQLException;
+    Provider findProviderByFullName(String fullName) throws SQLException;
+>>>>>>> Stashed changes
 }
