2635cf25163c9adec22622d93cd1bb7ebe40452e
Ted2121 <89663563+Ted2121@users.noreply.github.com>
2022-04-14 12:18:35 +0200

    Created SaleOrderDaoImplementation

--- src/db_access/DaoInterfaces/SaleOrderDao.java
+++ src/db_access/DaoInterfaces/SaleOrderDao.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface SaleOrderDao {
-    SaleOrder findSaleOrderById(int saleOrderId) throws SQLException;
-    List<SaleOrder> findAllSaleOrders() throws SQLException;
+    SaleOrder findSaleOrderById(int saleOrderId, boolean retrieveCustomer, boolean retrieveLineItem) throws SQLException;
+    List<SaleOrder> findAllSaleOrders(boolean retrieveCustomer, boolean retrieveLineItem) throws SQLException;
     void createSaleOrder(SaleOrder objectToInsert) throws SQLException;
     void updateSaleOrder(SaleOrder objectToUpdate) throws SQLException;
     void deleteSaleOrder(SaleOrder objectToDelete) throws SQLException;
