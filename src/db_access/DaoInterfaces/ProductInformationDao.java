9a91eae067b2d71ac8571268c465382b964edffc
Kylian CONNAN <10435524@ucn.dk>
2022-04-14 16:06:28 +0200

    Change to list

--- src/db_access/DaoInterfaces/ProductInformationDao.java
+++ src/db_access/DaoInterfaces/ProductInformationDao.java
@@ -5,12 +5,13 @@ import model.ProductInformation;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ProductInformationDao {
     void createProductInformation(ProductInformation objectToInsert) throws SQLException;
     void updateProductInformation(ProductInformation objectToUpdate) throws SQLException;
     void deleteProductInformation(ProductInformation objectToDelete) throws SQLException;
-    ArrayList<ProductInformation> findAllProductInformationEntries() throws SQLException;
+    List<ProductInformation> findAllProductInformationEntries() throws SQLException;
     ProductInformation findProductInformationByProduct(Product product) throws SQLException;
     ProductInformation findProductInformationByProductName(String productName) throws SQLException;
     ProductInformation findProductInformationByProductId(int productId) throws SQLException;
