7e4d1cb205f24538d1c5454cb36adbac93ef33ee
schesneau <10435523@ucn.dk>
2022-04-17 14:56:09 +0200

    Test of the controller

---
+++ src/test/ControllerTests/TestCreatePurchaseOrderControllerImplementation.java
@@ -0,0 +1,56 @@
+package test.ControllerTests;
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import controller.ControllerFactory;
+import controller.ControllerImplementation.CreatePurchaseOrderControllerImplementation;
+import controller.ControllerInterfaces.CreatePurchaseOrderController;
+import db_access.DaoFactory;
+import model.LineItem;
+import model.Product;
+import model.Provider;
+import model.PurchaseOrder;
+
+public class TestCreatePurchaseOrderControllerImplementation {
+	private static CreatePurchaseOrderController controller;
+	
+	@BeforeClass
+	public static void setUp() {
+		controller = ControllerFactory.getCreatePurchaseOrderController();
+	}
+	
+	@Test
+	public void testCreatePurchaseOrderTransaction() throws Exception {
+		int numberOfPurchaseOrderBeforeTest = DaoFactory.getPurchaseOrderDao().findAllPurchaseOrders(false, false).size();
+		
+		for(Product product : DaoFactory.getProductDao().findAllProducts()) {
+			controller.addProductToPurchaseOrder(product, 3);
+		}
+		Provider provider = DaoFactory.getProviderDao().findAllProviders(false).get(0);
+		
+		controller.createPurchaseOrder(provider);
+		int numberOfPurchaseOrderAfterTest = DaoFactory.getPurchaseOrderDao().findAllPurchaseOrders(false, false).size();
+		
+		assertNotEquals("Should retrieve 1 more PurchaseOrder", numberOfPurchaseOrderBeforeTest, numberOfPurchaseOrderAfterTest);
+	}
+	
+	@AfterClass
+	public static void cleanUp() throws SQLException, Exception {
+		CreatePurchaseOrderControllerImplementation controllerImplementation = (CreatePurchaseOrderControllerImplementation) controller;
+		PurchaseOrder purchaseOrder = controllerImplementation.getPurchaseOrder();
+		for(LineItem lineItem : DaoFactory.getLineItemDao().findLineItemsByOrder(purchaseOrder, false)) {
+			lineItem.setOrder(purchaseOrder);
+			DaoFactory.getLineItemDao().deleteLineItem(lineItem);
+		}
+		
+		DaoFactory.getPurchaseOrderDao().deletePurchaseOrder(purchaseOrder);
+		DaoFactory.getOrderDao().deleteOrder(purchaseOrder);
+		
+	}
+}
