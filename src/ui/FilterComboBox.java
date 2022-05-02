53894db1fd1bf42d2494f5636e8b596d78e8dcdb
gaborkiss4 <93514570+gaborkiss4@users.noreply.github.com>
2022-05-02 17:06:39 +0200

    filtercombobox

---
+++ src/ui/FilterComboBox.java
@@ -0,0 +1,90 @@
+package ui;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ * A class for filtered combo box.
+ */
+public class FilterComboBox
+    extends JComboBox
+{
+    /**
+     * Entries to the combobox list.
+     */
+    private List<String> entries;
+
+    public List<String> getEntries()
+    {
+        return entries;
+    }
+
+    public FilterComboBox(List<String> entries)
+    {
+        super(entries.toArray());
+        this.entries = entries  ;
+        this.setEditable(true);
+
+        final JTextField textfield =
+            (JTextField) this.getEditor().getEditorComponent();
+
+        /**
+         * Listen for key presses.
+         */
+        textfield.addKeyListener(new KeyAdapter()
+        {
+            public void keyReleased(KeyEvent ke)
+            {
+                SwingUtilities.invokeLater(new Runnable()
+                {
+                    public void run()
+                    {
+                        /**
+                         * On key press filter the list.
+                         * If there is "text" entered in text field of this combo use that "text" for filtering.
+                         */
+                        comboFilter(textfield.getText());
+                    }
+                });
+            }
+        });
+
+    }
+
+    /**
+     * Build a list of entries that match the given filter.
+     */
+    public void comboFilter(String enteredText)
+    {
+        List<String> entriesFiltered = new ArrayList<String>();
+
+        for (String entry : getEntries())
+        {
+            if (entry.toLowerCase().contains(enteredText.toLowerCase()))
+            {
+                entriesFiltered.add(entry);
+            }
+        }
+
+        if (entriesFiltered.size() > 0)
+        {
+            this.setModel(
+                    new DefaultComboBoxModel(
+                        entriesFiltered.toArray()));
+            this.setSelectedItem(enteredText);
+            this.showPopup();
+        }
+        else
+        {
+            this.hidePopup();
+        }
+    }
+}
